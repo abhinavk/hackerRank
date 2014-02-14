3dcb519a9fa370aaa641ddde5d065bb8f6ed4d3d
Abhinav Kumar <abhinavkumar@outlook.com>
2014-02-14 10:06:16 +0530

    Angry Children

---
+++ algorithms/angry_children.py
@@ -0,0 +1,13 @@
+# HackerRank - Algorithms - Warmup
+# Angry Children
+
+nL = int(input())
+k = int(input())
+n = [int(input()) for _ in range(nL)]
+n.sort()
+best = n[k-1] - n[0]
+for i in range(nL-k+1):
+    if n[i+k-1] - n[i] < best:
+        best = n[i+k-1] - n[i]
+
+print(best)
