422f77dd45506cc66d53bb864f4385b82a6685f2
Abhinav Kumar <abhinavkumar@outlook.com>
2014-02-14 21:58:40 +0530

    Create choco_feast.py

---
+++ algorithms/choco_feast.py
@@ -0,0 +1,19 @@
+# HackerRank - Algorithms - Warmup
+# Chocolate Feast
+
+
+def how_many_chocolates(n, c, m):
+    wrappers = choco = n//c
+    while(wrappers >= m):
+        exch = wrappers//m
+        choco += exch
+        wrappers = wrappers - exch*m + exch
+    return choco
+
+
+test_cases = int(input())
+for _ in range(test_cases):
+    inp = input()
+    s = inp.split(' ')
+    n, c, m = tuple(s)
+    print(how_many_chocolates(int(n), int(c), int(m)))
