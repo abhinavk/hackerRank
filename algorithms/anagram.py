535594b3f3178d7b1d1c311ea584ee5bb8531db0
Abhinav Kumar <abhinavkumar@outlook.com>
2014-02-15 15:35:13 +0530

    Anagram
    HackerRank - Algorithms - Strings
    Anagrams

---
+++ algorithms/anagram.py
@@ -0,0 +1,31 @@
+# HackerRank - Algorithms - Strings
+# Anagrams
+
+
+def shifts_required(input_str):
+    shifts = 0
+    if len(input_str) % 2:
+        return -1
+    a, b = input_str[:len(input_str)//2], input_str[len(input_str)//2:]
+    dict = {}
+    dicta = {}
+    for i in b:
+        if i in dict.keys():
+            dict[i] += 1
+        else:
+            dict[i] = 1
+            dicta[i] = 0
+    for i in a:
+        if i in dicta.keys():
+            dicta[i] += 1
+        else:
+            dicta[i] = 1
+    for i in dict.keys():
+        if dict[i] > dicta[i]:
+            shifts += dict[i]-dicta[i]
+    return shifts
+
+
+t = int(input())
+for _ in range(t):
+    print(shifts_required(input()))
