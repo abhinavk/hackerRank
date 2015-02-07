f8f04ddb39a31182e8a1e0120d7f1003e2ea9e4a
Abhinav Kumar <abhinavkumar@outlook.com>
2015-02-07 11:38:19 +0530

    Added C++ implementation of Alternating Characters

---
+++ alternatingchars.cpp
@@ -0,0 +1,38 @@
+/*
+ * HackerRank - Algorithms - Warmup
+ * Alternating Characters
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string a;
+        cin>>a;
+        char last = a.at(0);
+        int _count = 0;
+        int fcount = 0;
+        for(int i=1;i<a.length();i++)
+        {
+            if(a.at(i) != last)
+            {
+                fcount += _count;
+                last = a.at(i);
+                _count = 0;
+            }
+            else
+            {
+                _count++;
+            }
+        }
+        fcount += _count;
+        cout<<fcount<<endl;
+    }
+}
