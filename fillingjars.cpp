f0bd2ce8e06b523321a94bd33713f69a41c467ee
Abhinav Kumar <abhinavkumar@outlook.com>
2015-02-12 22:24:38 +0530

    Added filling jars implementation

---
+++ fillingjars.cpp
@@ -0,0 +1,25 @@
+/*
+ * HackerRank - Algorithms - Warmup
+ * Filling Jars
+ *
+ * Abhinav Kumar
+ * abhinavk.me
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long int n,m,total=0;
+    cin>>n>>m;
+    for(long int i=0;i<m;i++)
+    {
+        long a,b,c;
+        cin>>a>>b>>c;
+        total += (b-a+1)*c;
+    }
+    cout<<total/n;
+    return 0;
+}
