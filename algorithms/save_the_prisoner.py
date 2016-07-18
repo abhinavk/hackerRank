d0d3c349225683956e95cf9b8607a7c267fe4e60
Abhinav Kumar <abhinav@outlook.com>
2016-07-18 23:32:47 +0530

    Added solution of Save the Prisoner

---
+++ algorithms/save_the_prisoner.py
@@ -0,0 +1,19 @@
+'''
+HackerRank - Algorithms - Implementation
+Save the Prisoner
+'''
+
+def solve_case():
+    n,m,s = input().strip().split(' ')
+    n,m,s = [int(n),int(m),int(s)]
+    if m > n:
+        m = m % n
+    last = s - 1 + m
+    if last > n:
+        print(str(last-n))
+    else:
+        print(str(last))
+t = int(input())
+
+for _ in range(t):
+    solve_case()
