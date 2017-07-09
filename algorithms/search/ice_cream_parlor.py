f78efd95636a19f995b4560c0988ce9733a4beaa
Abhinav Kumar <abhinav@outlook.com>
2017-07-09 14:19:55 +0530

    Added solution for Algorithms\Search\Ice Cream Parlor

---
+++ algorithms/search/ice_cream_parlor.py
@@ -0,0 +1,25 @@
+"""
+HackerRank - Algorithm - Search
+Ice Cream Parlor
+"""
+
+
+def solve_problem():
+    m = int(input())
+    n = int(input())
+    c = map(int, input().split(' '))
+
+    hashmap = {}
+
+    for i, ic in enumerate(c):
+        rem = m-ic
+        if rem in hashmap:
+            print(' '.join(sorted([hashmap[rem], i])))
+        else:
+            hashmap[rem] = i
+
+
+if __name__ == '__main__':
+    testcase = int(input())
+    for _ in range(testcase):
+        solve_problem()
